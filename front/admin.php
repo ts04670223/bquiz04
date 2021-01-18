c6fac684914fd2f58a97ad5a7ee446f4746be38f
johy <ts04670223@gmail.com>
2021-01-18 14:35:09 +0800

    完成管理者登入

--- front/admin.php
+++ front/admin.php
@@ -11,10 +11,18 @@
   </tr>
   <tr>
     <td class="tt">驗證碼</td>
-    <td class="pp"><input type="text" name="ans" id="ans"></td>
+    <td class="pp">
+    <?php
+      $a = rand(10, 99);
+      $b = rand(10, 99);
+      $_SESSION['ans'] = $a + $b;
+      echo $a . "+" . $b . "=";
+
+      ?>
+    <input type="text" name="ans" id="ans"></td>
   </tr>
 </table>
 <div class="ct">
 
-<button onclick="login()">確認</button>
+<button onclick="login('admin')">確認</button>
 </div>
