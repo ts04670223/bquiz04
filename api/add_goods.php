8712781802a37699ce1fbef5bf0c697f45ef4237
Johny <ts04670223@gmail.com>
2021-06-16 21:34:31 +0800

    調整圖片api

--- api/add_goods.php
+++ api/add_goods.php
@@ -1,6 +1,7 @@
 <?php include_once "../base.php";
 
 if(!empty($_FILES['img']['tmp_name'])){
+  move_uploaded_file($_FILES['img']['tmp_name'],'../img/'.$_FILES['img']['name']);
   $_POST['img']=$_FILES['img']['name'];
 }
 
